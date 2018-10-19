ee1d60740257624768bb2e26f52f3fc6789b9ea3
Sam-GN <email@example.comsam.ganjinishabouri@calbaptist.edugit push -u origin mastergit config --global user.email email@example.comsam.ganjinishabouri@calbaptist.edu>
2018-10-18 19:07:16 -0700

    stack and linked list added

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,36 +1,94 @@
 import org.junit.Test;
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class MainTest {
 
+     ArrayList<Integer> myList;
+     Stack<Integer> myStack;
+     LinkedList<Integer> myLinkedList;
+     Queue<Integer> myq = new LinkedBlockingQueue<Integer>();  
 
+
+     @BeforeEach
+     public void doStuff(){
+         myList = new ArrayList<Integer>();
+     }
 
     @Test
     public void notNull(){
-        Assertions.assertNotNull(main.list);
+        myList = new ArrayList<Integer>();
+        Assertions.assertNotNull(myList);
+
+        myStack = new Stack<Integer>();
+        Assertions.assertNotNull(myStack);
+
+        myLinkedList = new LinkedList<Integer>();
+        Assertions.assertNotNull(myLinkedList);
+
     }
 
     @Test
     public void add() {
-        Assertions.assertTrue(main.add(1));
+        myList = new ArrayList<Integer>();
+        Assertions.assertTrue(myList.add(1));
+
+        myStack = new Stack<Integer>();
+        Assertions.assertTrue(myStack.add(1));
+
+        myLinkedList = new LinkedList<Integer>();
+        Assertions.assertTrue(myLinkedList.add(1));
     }
 
     @Test
     public void contains(){
-        main.add(1);
-        Assertions.assertTrue(main.contain(1));
+        myList = new ArrayList<Integer>();
+        myList.add(1);
+        Assertions.assertTrue(myList.contains(1));
+
+        myStack = new Stack<Integer>();
+        myStack.add(1);
+        Assertions.assertTrue(myStack.contains(1));
+
+        myLinkedList = new LinkedList<Integer>();
+        myLinkedList.add(1);
+        Assertions.assertTrue(myLinkedList.contains(1));
 
     }
 
+    @Test
     public void size(){
-        main.add(1);
-        Assertions.assertEquals(1,main.size());
+        myList = new ArrayList<Integer>();
+        myList.add(1);
+        Assertions.assertEquals(1,myList.size());
+
+        myStack = new Stack<Integer>();
+        myStack.add(1);
+        Assertions.assertEquals(1,myStack.size());
+
+        myLinkedList = new LinkedList<Integer>();
+        myLinkedList.add(1);
+        Assertions.assertEquals(1,myLinkedList.size());
     }
 
     @Test
     public void remove() {
-        main.add(1);
-        Assertions.assertEquals(1,main.remove(0));
+        myList = new ArrayList<Integer>();
+        myList.add(1);
+        Assertions.assertEquals((Integer) 1,myList.remove(0));
+
+        myStack = new Stack<Integer>();
+        myStack.add(1);
+        Assertions.assertEquals((Integer) 1,myStack.pop());
+
+        myLinkedList = new LinkedList<Integer>();
+        myLinkedList.add(1);
+        Assertions.assertEquals((Integer) 1,myLinkedList.remove(0));
     }
 
 }
